d8b5a2df9195d238b96f278d1cc904d442f9f63f
Sarbajit Karmakar <sarbajitkarmakar26@gmail.com>
2024-03-30 14:21:55 +0530

    set up HTTP request

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = 'https://v2.jokeapi.dev/joke/Any';
+
+
+app.get('/', async (req , res)=>{
+    try {
+        const result = await axios.get(API_URL);
+        res.render("index.ejs",{
+            result: JSON.stringify(result.data),
+        })
+    } catch (error) {
+       res.send(error.message); 
+    }
+})
+
+app.listen(port , () =>{
+    console.log(`Website is running on ${port} port`);
+});
